7aa5dbb978ea2e98dec298068c59086ab20dba7a
xoghk9zla <xorbs9rla@naver.com>
2018-06-10 21:29:04 +0900

    .

--- 2018 STL/2018 STL/Test.cpp
+++ 2018 STL/2018 STL/Test.cpp
@@ -1,71 +1,141 @@
 #include <iostream>
+
 #include <functional>
+
 #include <vector>
+
 #include <map>
+
 #include <string>
+
 #include <fstream>
+
 #include <algorithm>
+
+
 
 using namespace std;
 
+
+
 vector<string> temp;
+
 map<char, int> spelling_frequency;
+
 map<string, int> sort_by_word;
+
 map<int, string> word_frequency;
+
+
 
 void load();
 
+
+
 void main() {
+
 	// 1) 파일을 읽어 각 철자의 빈도수 표시
+
 	// 2) 가장 많이 출현한 단어순 출력
+
 	// 3) 길이가 가장 긴 단어 출력
+
 	load();
 
+
+
 	while (1) {
+
 		system("cls");
+
 		cout << "1. 각 철자의 빈도수 출력 2. 가장 많이 출현한 단어순 출력" << endl;
+
 		int option;
+
 		cin >> option;
 
+
+
 		switch (option)
+
 		{
+
 		case 1:
+
 			show_frequency();
+
 			break;
+
 		case 2:
+
 			cout << word_frequency[word_frequency.size() - 1].c_str() << endl;
+
 			for (auto& s : word_frequency)
+
 				cout << "[\t" << s.first << "\t]: " << s.second.c_str() << endl;
+
 			break;
+
 		default:
+
 			break;
+
 		}
+
 		system("pause");
+
 	}
+
 }
+
+
 
 void load() {
 
+
+
 	fstream in("이상한 나라의 앨리스.txt");
+
 	// 이 파일은 UTF-8 형식의 파일로 BOM(Byte order mark)이 기록되어 있다.
 
+
+
 	char c;
+
 	in >> c >> c >> c; // BOM무시
+
+
 
 	map<string, int> simap;
 
+
+
 	string s;
+
 	while (in >> s) {
+
 		simap[s]++;	// cimap.operator[](c)
+
 	}
 
+
+
 	// 가장 길이가 긴 단어는? (단어는 공백으로 분리됨)
+
 	// STL에서 가능하면 for loop를 사용하지 않는다.( 대신할 알고리즘이 있는 경우)
+
 	auto p = max_element(simap.begin(), simap.end(), [](const auto& a, const auto& b) {
+
 		return a.first < b.first;
+
 	});
+
 	if (p != simap.end())
+
 		cout << p->first << endl;
 
+
+
 	system("pause");
+
 }
--- 2018 STL/2018 STL/소스.cpp
+++ 2018 STL/2018 STL/소스.cpp
@@ -1,40 +1,38 @@
 //----------------------------------------------------------------------
-// 2018. 1. STL 월 23, 화 56	6월 4일	(13주 1일차)
+// 2018. 1. STL 월 23, 화 56	6월 5일	(13주 2일차)
 // 
 // 9장 알고리즘
 // 1. 원소를 수정하지 않는 알고리즘()
 // 2. 원소를 수정하는 알고리즘()
 // 3. 정렬과 관련된 알고리즘
+//		partition - 조건에 맞는 것과 아닌것을 분리한다.
+//		sort - 전체를 기준(기본 < )에 따라 정렬 한다.
+//		partial_sort - 특정 위치까지만 정렬한다.
+//		nth_
 // 
 //----------------------------------------------------------------------
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <thread>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <functional>
 #include "save.h"
 
 using namespace std;
 
 void main() {
+	vector<int> v(10000);
+	iota(v.begin(), v.end(), 1);
+	random_shuffle(v.begin(), v.end());
 
-	string s{ "abcdefghijklmnopqrstuvwxyz " };
+	// v를 오름차순으로 정렬하라.
+	sort(v.begin(), v.end());
 
-	while (1)
-	{
-		system("cls");
-		for (int i = 0; i < 10; ++i) {
-			cout << endl;
-		}
-
-		cout << "\t\t";
-
-		rotate(s.rbegin(), s.rbegin() + 1, s.rend());
-		cout << s << endl;
-		this_thread::sleep_for(30ms);
-	}
-
+	// v에서 7777이라는 값이 있는지 찾아보고 어디에 있는지(몇 등인지) 출력해보라.
+	cout << boolalpha << binary_search(v.begin(), v.end(), 7777) << endl;
+	equal_range(v.begin(), v.end(), 9777);
 
 	save();
 }
